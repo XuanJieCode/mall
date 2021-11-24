a90d83976e24f53d806d0c84193cdc8f9a8fb97a
XuanJieCode <mrqin.ch@foxmail.com>
2021-11-24 22:07:34 +0800

    master

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.export = {
+	resolve: {
+		alias:{
+			'assets': '@/assets',
+			'common': '@/common',
+			'components': '@/components',
+			'network': '@/network',
+			'views': '@/views'
+		}
+	}
+}
